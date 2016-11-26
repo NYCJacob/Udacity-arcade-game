15a457f262eaf2d04eac2b1e8b47690a2a6df215
jsherman <jacob@jbsherman.com>
2016-11-26 18:00:09 -0500

    player and three enemies move correctly, TODO: collision detection

--- js/app.js
+++ js/app.js
@@ -1,25 +1,41 @@
+// global random int generator
+function randomInt(min, max) {
+    var rand = Math.random() * (max - min + 1);
+    rand = Math.floor(rand) + min;
+    return rand;
+}
+
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     // random starting position of enemy
     // TODO:  need to figure out coord boudaries and y rows
     // canvas dimensions are set at width = 505; height = 606
-    this.x = Math.random() * 500;
-    // estimate y boundaries for enemy area
-    this.y = Math.random() * 300;
+    // Math.floor to get random int
+    this.minX = 0;
+    // canvas width = 505
+    this.maxX = 505;
+    // min needs to clear river at top therefore not 0
+    this.minY = 15;
+    // maxY needs to stay above player area  canvas height = 606
+    this.maxY = 230;
+    this.x = randomInt(this.minX, this.maxX);
+    this.y = randomInt(this.minY, this.maxY);
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    //console.log(this + this.x + this.y);
+    if (this.x >= 500) {
+        this.x = -20;
+        this.y = randomInt(this.minY, this.maxY);
+    } else {
+        this.x += 50 * dt;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -45,7 +61,7 @@ Player.prototype.handleInput = function (pressedKey) {
     if(pressedKey === 'up' && this.y > 0){
         this.y -= 25;
     }
-    if(pressedKey === 'right' && this.x < 425){
+    if(pressedKey === 'right' && this.x < 400){
         this.x += 50;
     }
     if(pressedKey === 'down' && this.y < 425){
@@ -53,9 +69,8 @@ Player.prototype.handleInput = function (pressedKey) {
     }
 };
 
-Player.prototype.update = function (dt) {
+Player.prototype.update = function () {
     // player update code
-    console.log("this is the Player.proto.update  " + dt);
 };
 
 Player.prototype.render = function () {
